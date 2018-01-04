c58ef1a614e6eedcb33d59f7524a6cf96548ff32
Ed Summers <ehs@pobox.com>
2018-01-04 16:21:12 -0500

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+An experiment in viewing images from the [Lakeland](https://lakeland.mith.us)
+Omeka site.
---
+++ scripts/copy.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+# A hackish script to extract images and their metadata from a snapshot
+# of the Lakeland Omeka data at s3://mith-lakeland-data
+#
+# resulting metadata and image files will be written to the static dir
+# in the project directory
+
+import os
+import json
+
+from shutil import copyfile
+from os.path import join, isdir, dirname
+
+data_dir = '/Users/ed/Data/mith-lakeland-data'
+output_dir =  join(dirname(__file__), '../static')
+
+items = []
+
+def metadata(item):
+    m = {
+        'id': item['id'],
+        'created': item['added'],
+        'published': item.get('public', False)
+    }
+    for et in item['element_texts']:
+        name = et['element']['name'].lower().replace(' ', '_')
+        m[name] = et['text']
+    return m
+
+def copy_files(path, item):
+    files_dir = join(path, 'files')
+    if not isdir(files_dir):
+        return False
+    file_id = os.listdir(files_dir)[0]
+    file_path = join(files_dir, file_id)
+    if isdir(file_path):
+        files = os.listdir(file_path)
+        if ('fullsize.jpg' in files):
+            dest = join(output_dir, str(item['id']))
+            if not isdir(dest):
+                os.mkdir(dest)
+            for file in files:
+                copyfile(join(file_path, file), join(dest, file))
+        return True
+    # Exif Array
+    return False
+
+for dirpath, dirnames, filenames in os.walk(data_dir):
+    for filename in filenames:
+        if filename == 'item.json':
+            item = json.load(open(join(dirpath, filename)))
+            if not item or item['item_type'] == None:
+                continue
+            if item['item_type']['name'] != 'Still Image':
+                continue
+            found = copy_files(dirpath, item)
+            if found:
+                items.append(metadata(item))
+
+json.dump(items, open(join(output_dir, 'items.json'), 'w'), indent=2)
